0dfad82edf2fb440351b25b6f5a3e697c71e3140
MADHIKATLAradhika <130963480+MADHIKATLAradhika@users.noreply.github.com>
2023-08-30 11:05:43 +0530

    calculatorUsingHTML,CSS,JAVASCRIPT

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-$(document).ready(function() {
-     $('#menu').click(function(){
-        $(this).toggleClass('fa-items');
-        $('header').toggleClass('toggle');
+const display=document.querySelector(".display");
+const buttons=document.querySelectorAll("button");
+const specialChars=["%","/","*","-","+","="];
+let output="";
 
+const calculate=(btnvalue)=>{
+    if(btnvalue == "=" && btnvalue !== ""){
+       output=eval(output.replace("%","/100"));
+    }
+    else if(btnvalue==="AC"){
+        output=""
+    }
+    else if(btnvalue === "DEL"){
+        output=output.toString().slice(0,-1);
+    }
+    else{
+         if(output === "" && specialChars.includes(btnvalue))
+          return;
+        output += btnvalue;
+    }
+   display.value=output;
+};
 
-    });
-    $(window).on('scroll load',function(){
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
-        if($(window).scrollTop() > 0){
-            $('.top').show();
-        }
-        else{
-            $('.top').hide();
-        }
-
-    });
-    
-    $('a[href *= "#"]').click(function(e){
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop : $($(this).attr('href')).offset().top,
-        },
-        500,
-        'linear'
-        );
-
-    });
-});
+buttons.forEach((button) => {
+button.addEventListener("click",(e) => calculate(e.target.dataset.value));
+}
+);
